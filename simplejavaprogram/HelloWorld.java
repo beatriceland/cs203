66cca2651c6b4a902f42e762b132ec1603eb0d99
Beatrice Land <beland@vassar.edu>
2016-02-01 15:43:33 -0500

    First snapshot

---
+++ simplejavaprogram/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Beatrice Land
+ * 
+ * File: HelloWorld.java
+ * ---------------------
+ * This program prints 
+ * 
+ *      Hello world!!
+ */
+public class HelloWorld {  // class declaration line
+   
+   public static void main(String[] args) {   // main method signature
+       // The line below prints its String argument followed by a 
+       // newline.
+       System.out.println("Hello World!!"); 
+   }
+}  /* end of class HelloWorld */
